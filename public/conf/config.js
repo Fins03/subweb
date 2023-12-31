54b040547589fa15ccb97cffa94cdb41198279c2
Fins03 <42291925+Fins03@users.noreply.github.com>
2023-12-31 23:47:22 +0800

    Update config.js

--- public/conf/config.js
+++ public/conf/config.js
@@ -1,14 +1,14 @@
 window.config = {
   // 网站标题
-  siteName: 'Subconverter Web',
+  siteName: 'fins Web',
   // API 地址
   apiUrl: 'http://127.0.0.1:25500',
   // 短域名服务地址
   shortUrl: 'https://s.ops.ci',
   // 默认远程配置
-  remoteConfigName: '默认配置',
+  remoteConfigName: 'hhgffdd',
   // 默认远程配置地址
-  remoteConfigUrl: '',
+  remoteConfigUrl: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_AdblockPlus.ini',
   // 首页菜单
   menuItem: [
     {
